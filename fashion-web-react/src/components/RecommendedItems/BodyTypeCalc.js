23e9d79c0060d24e1a4160250177c4efb4128872
Metra Strode <126508047+Matra888@users.noreply.github.com>
2023-09-17 11:03:36 +0300

    Update BodyTypeCalc.js

--- fashion-web-react/src/components/RecommendedItems/BodyTypeCalc.js
+++ fashion-web-react/src/components/RecommendedItems/BodyTypeCalc.js
@@ -1,14 +1,29 @@
-// Body Type Test calculation
+// BodyTypeCalc.js
 
-// Perform your body type calculation and export the result.
-// For example:
-const BodyTypeCalc = () => {
+const BodyTypeCalc = (shoulder, bust, waist, hips) => {
+  // Initialize bodyShape variable to store the result
+  let bodyShape = "";
+
   // Your actual calculation logic here.
-  // When ready, return the calculated body type as a string.
+  if (shoulder > hips + 2 || bust > hips + 4) {
+    bodyShape = "Inverted Triangle";
+  } else if (hips > shoulder + 2 || hips > bust + 4) {
+    bodyShape = "Triangle";
+  } else if (waist > bust + 2 || waist > hips + 2) {
+    bodyShape = "Circle";
+  } else if (Math.abs(shoulder - hips) < 8 && Math.abs(bust - hips) < 8 && Math.abs(waist - hips) < 8) {
+    bodyShape = "Rectangle";
+  } else if (Math.abs(shoulder - hips) < 8 && Math.abs(bust - hips) < 8 && Math.abs(waist - hips) >= 8) {
+    bodyShape = "Hourglass";
+  } else {
+    bodyShape = "Body shape is not clearly defined.";
+  }
 
-  return "Inverted_triangle"; // Replace with the actual calculation result
+  // When ready, return the calculated body type as a string.
+  return bodyShape;
 
   // Names of all body types are defined in the file RecommendedItemsData.js
 };
 
 export default BodyTypeCalc;
+
