3423c284debff2c204eb0219499bcd5e31c2a287
CharisDev <99430685+CharisDev@users.noreply.github.com>
2023-09-20 13:03:43 +0300

    create separate body type test results component

--- fashion-web-react/src/pages/YourBodyType/YourBodyType.jsx
+++ fashion-web-react/src/pages/YourBodyType/YourBodyType.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import BodyTypeForm from "../../components/BodyTypeForm/BodyTypeForm";
+import "./YourBodyType.scss";
 
 const YourBodyType = () => {
   return (
     <main>
+      {/* display logic (show test form or show test result) to be moved to this page */}
       <BodyTypeForm />
     </main>
   );
